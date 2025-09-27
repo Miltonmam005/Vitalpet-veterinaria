dfbc63cd15316144c46515e890b66f8fb50e6bd5
Milton Mamani <miltin.mamani010@gmail.com>
2025-09-27 15:34:11 -0300

    inserte las etiquetas a para que se vean los link en la parte del footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,51 +1,66 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
-return (
-    <footer className="colorFoter text-center text-lg-startp-3">
+  return (
+    <footer className="bg-dark text-center text-lg-startp-3">
       <Container className="text-center text-white">
         <Row>
-              <Col
+          <Col
             md={4}
             className="mb-4 mb-md-0 d-flex flex-column align-items-center align-items-md-start"
           >
             <h1 className="titulofoter">VitalPet</h1>
-            <p className="m-0 textocolor">
-              &copy; Xtream. Todos los derechos reservados.
+            <p className="m-0 text-white">
+              &copy; VitalPet veterinaria. Todos los derechos reservados.
             </p>
           </Col>
-            <Col md={4} className="mb-4 mb-md-0">
-            <h5 className="text-uppercase titulofoter fw-bold mb-3">Enlaces Rápidos</h5>
-            <ul className="list-unstyled">
-              <li className='textocolor'>
-                Contacto
+          <Col md={4} className="mb-4 mb-md-0">
+            <h5 className="text-uppercase titulofoter fw-bold mb-3">
+              Enlaces Rápidos
+            </h5>
+            <ul className="list-unstyled ">
+              <li className="textocolor">
+                <a href="" className="text-decoration-none text-white ">
+                  Contacto
+                </a>
               </li>
-              <li className='textocolor'>
-                Acerca de nosotros
+              <li className="textocolor">
+                <a href="" className="text-decoration-none text-white">
+                  Acerca de nosotros
+                </a>
               </li>
-              <li className='textocolor'>
-                Subscripciones 
+              <li className="textocolor">
+                <a href="" className="text-decoration-none text-white">
+                  Subscripciones
+                </a>
               </li>
-              <li className='textocolor'>
-               Contenido Premium
+              <li className="textocolor">
+                <a href="" className="text-decoration-none text-white">
+                  Solicituides de Plan
+                </a>
               </li>
             </ul>
           </Col>
-       
           <Col md={4}>
-            <h5 className="text-uppercase titulofoter fw-bold mb-3">Síguenos</h5>
-            <div className="d-flex justify-content-center justify-content-md-start textocolor">
-              <a href="https://facebook.com/Xtream" target="_blank" rel="noopener noreferrer" className="text-reset me-3 footer-icon-link">
-                <i className="bi bi-facebook fs-4"></i>
-              </a>
-              <a href="https://twitter.com/Xtream" target="_blank" rel="noopener noreferrer" className="text-reset me-3 footer-icon-link">
-                <i className="bi bi-twitter fs-4"></i>
-              </a>
-              <a href="https://instagram.com/Xtream" target="_blank" rel="noopener noreferrer" className="text-reset me-3 footer-icon-link">
-                <i className="bi bi-instagram fs-4"></i>
-              </a>
-            </div>
+            <h5 className=" titulofoter fw-bold mb-3">Síguenos</h5>
+            {/* <div className="d-flex justify-content-center justify-content-md-start "> */}
+            <a href="" className="text-decoration-none text-white">
+              {" "}
+              <i className="bi bi-facebook fs-4"></i>{" "}
+            </a>
+            <a href="" className="text-decoration-none text-white">
+              {" "}
+              <i className="bi bi-twitter fs-4"></i>{" "}
+            </a>
+            <a href="" className="text-decoration-none text-white">
+              {" "}
+              <i className="bi bi-instagram"></i>{" "}
+            </a>
+            <a href="" className="text-decoration-none text-white">
+              {" "}
+              <i className="bi bi-whatsapp"></i>{" "}
+            </a>
+            {/* </div> */}
           </Col>
         </Row>
       </Container>
@@ -54,4 +69,3 @@ return (
 }
 
 export default Footer;
-
