02309a963b6ca118a0b601a3fddd24217dbd24a5
Milton Mamani <miltin.mamani010@gmail.com>
2025-09-27 12:08:17 -0300

    agrege el nav con sus calses y sus aciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './components/shared/Menu'
 
 function App() {
   
 
   return (
     <>
-   <h1>Bienvenidos a la veterinaria VitalPet</h1>
+    <> <Menu></Menu>
+     <main className='container my-4'>
+    <h1 className='text-center'>Bienvenidos a la veterinaria VitalPet</h1>
+    
+    </main>
+   <footer className='bg-darck text-light text-center py-3'>
+    <p> Todos los derechos reservados</p>    
+    </footer>  
+    </>
+   
     </>
   )
 }
--- src/components/Shared/Menu.jsx
+++ src/components/Shared/Menu.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Menu = () => {
     return ( <>
-    {/* aca ira en navbart */}
+    <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
+      <Container fluid>
+        <Navbar.Brand href="#">VitalPet</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Inicio</Nav.Link>
+            <Nav.Link href="#action2">Loguin</Nav.Link>
+            <Nav.Link href="#action3">Registro</Nav.Link>
+            <Nav.Link href="#action4">Solicitar Turno</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar>
     </>    
     );
 };
