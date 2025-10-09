a31c3fbc008b8f26494e4dfa27aab8f74fe0f5b0
Celina <idkmahee@gmail.com>
2025-10-08 21:08:35 -0300

    agregue archivo login y carpeta estilos y modifique las rutas para el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from './components/shared/Menu.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router';
-import SobreNosotros from './components/pages/sobreNosotros.jsx';
-import Footer from "./components/shared/Footer.jsx"
-import Inicio from './components/Inicio';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./components/shared/Menu.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import SobreNosotros from "./components/pages/sobreNosotros.jsx";
+import Footer from "./components/shared/Footer.jsx";
+import Inicio from "./components/Inicio";
+import Login from "./components/pages/Login.jsx";
 function App() {
   return (
     <>
       <BrowserRouter>
-    <Menu/>
-      <Routes>
-      <Route path="/" element={<Inicio></Inicio>}></Route>
-      <Route path="/sobreNosotros" element={<SobreNosotros></SobreNosotros>}></Route>
-      </Routes>
-      <Footer/>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Inicio></Inicio>}></Route>
+          <Route
+            path="/sobreNosotros"
+            element={<SobreNosotros></SobreNosotros>}
+          ></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
